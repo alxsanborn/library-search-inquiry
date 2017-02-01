1e611af1bcc15822ef8522ab5c6bea5ea63edeb2
alxsanborn <alxsanborn@gmail.com>
2017-02-01 11:56:09 -0500

    store all instances of books

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+var allBooks = []
+
 function Library(title, author, year, isbn) {
   this.title = title,
   this.author = author,
@@ -5,8 +7,8 @@ function Library(title, author, year, isbn) {
   this.isbn = isbn,
   this.result = function(){
     return this
-  }
+  },
+  allBooks.push(this)
 }
-
 
 // We're after an ES5 Javascript class for a library which allows people to find book details by ISBN number and search for books by their title. Please add comments/thoughts about any limitations your implementations yours had and why you chose to do things a certain way.
