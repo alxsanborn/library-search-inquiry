62e953fc52d034382a0adebd572d785ea749e66c
alxsanborn <alxsanborn@gmail.com>
2017-02-01 11:47:21 -0500

    create constructor function with attributes

---
+++ index.js
@@ -0,0 +1,9 @@
+function Library(title, author, year, isbn) {
+  this.title = title,
+  this.author = author,
+  this.year = year,
+  this.isbn = isbn,
+}
+
+
+// We're after an ES5 Javascript class for a library which allows people to find book details by ISBN number and search for books by their title. Please add comments/thoughts about any limitations your implementations yours had and why you chose to do things a certain way.
