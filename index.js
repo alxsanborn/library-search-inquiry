3000b5a293f90e1d106fe5bf1c4ad8e38172feaf
alxsanborn <alxsanborn@gmail.com>
2017-02-01 13:41:52 -0500

    add title search function

--- index.js
+++ index.js
@@ -21,6 +21,16 @@ function isbnSearch(userIsbn) {
 }
 //I will optomize this with a better algorithm later, I just want to get it working.
 
+function titleSearch(userTitle) {
+  for (var book = 0; book < library.length; book++){
+    if (library[book].title == userTitle) {
+     return library[book].result() //what if there is more than one book with the same title?
+   }
+  }
+}
+
+//Future: a function that returns a book by author? a function that returns all of the books published in a given year? should these all be separate functions or is there a way to DRY this out?
+
 var book1 = new Book("blah", "blah", 333, 444)
 var book2 = new Book("meh", "meh", 111, 222)
 
